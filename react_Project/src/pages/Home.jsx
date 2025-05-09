d3e811297635623899d7ce24b9bcda8982e3c5ba
hosung jang <dndud0995@naver.com>
2025-05-09 20:23:27 +0900

    search update

--- react_Project/src/pages/Home.jsx
+++ react_Project/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MentorCard from "../components/MentorCard";
-
+import MentorSearch from "../components/MentorSearch";
 const Home = () => {
   return (
     <>
       <Header />
+      <MentorSearch/>
       <MentorCard />
       <Footer />
     </>
