c8156a9b6fe1f3d01217f2c750fd9669362d1045
DongEun02 <ehddms0517@naver.com>
2025-05-10 05:43:20 +0900

    서치바 구현(평점 제외)

--- react_Project/src/components/MentorList.jsx
+++ react_Project/src/components/MentorList.jsx
@@ -3,14 +3,32 @@ import MentorCard from "./MentorCard";
 import { MentorInfoContext } from "../App";
 import { useContext } from "react";
 
-const MentorList = () => {
+const MentorList = ({ filters }) => {
   const Data = useContext(MentorInfoContext);
+  let filtered = [...Data];
+  const { searchTerm, area, keyword } = filters;
+
+  if (searchTerm) {
+    filtered = filtered.filter((m) =>
+      m.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  if (area) {
+    filtered = filtered.filter((m) => m.city.name === area);
+  }
+
+  if (keyword) {
+    filtered = filtered.filter((m) =>
+      m.subjects.some((s) => s.name.includes(keyword))
+    );
+  }
 
   return (
     <div className="mentor-list">
-      <h2>멘토 목록 ({Data.length}명)</h2>
+      <h2>멘토 목록 ({filtered.length}명)</h2>
       <div className="mentor-grid">
-        {Data.map((mentor) => (
+        {filtered.map((mentor) => (
           <MentorCard key={mentor.id + Math.random()} mentor={mentor} />
         ))}
       </div>
--- react_Project/src/pages/Home.jsx
+++ react_Project/src/pages/Home.jsx
@@ -3,14 +3,26 @@ import Footer from "../components/Footer";
 import MentorList from "../components/MentorList";
 import MentorSearch from "../components/MentorSearch";
 import TitleNav from "../components/TitleNav";
+import { useState } from "react";
+
 const Home = () => {
+  const [filters, setFilters] = useState({
+    searchTerm: "",
+    area: "",
+    keyword: "",
+  });
+
+  const handleFilterChange = (newFilters) => {
+    setFilters((prev) => ({ ...prev, ...newFilters }));
+  };
+
   return (
     <>
       <Header />
       <main>
         <TitleNav />
-        <MentorSearch />
-        <MentorList />
+        <MentorSearch onFilterChange={handleFilterChange} />
+        <MentorList filters={filters} />
       </main>
       <Footer />
     </>
