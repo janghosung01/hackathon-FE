cc71883f5edc02432d74b728b676a92ef3d2fc8a
hosung jang <dndud0995@naver.com>
2025-05-09 23:02:12 +0900

    update MentorSearch

--- react_Project/src/components/MentorSearch.jsx
+++ react_Project/src/components/MentorSearch.jsx
@@ -13,10 +13,47 @@ const MentorSearch = () => {
         <span>주제</span>
         <span>정렬</span>
       </div>
+
       <div className="OptSelect">
-          <optgroup>
-            <select name="" id=""></select>
-          </optgroup>
+          <select name="Area" >
+            <option value="">선택</option>
+            <option value="SEOUL">서울특별시</option>
+            <option value="BUSAN">부산광역시</option>
+            <option value="DAEGU">대구광역시</option>
+            <option value="INCHEON">인천광역시</option>
+            <option value="GWANGJU">광주광역시</option>
+            <option value="DAEJEON">대전광역시</option>
+            <option value="ULSAN">울산광역시</option>
+            <option value="SEJONG">세종특별자치시</option>
+            <option value="GYEONGGI">경기도</option>
+            <option value="GANGWON">강원특별자치도</option>
+            <option value="CHUNGBUK">충청북도</option>
+            <option value="CHUNGNAM">충청남도</option>
+            <option value="JEOLLABUK">전북특별자치도</option>
+            <option value="JEOLLANAM">전라남도</option>
+            <option value="GYEONGBUK">경상북도</option>
+            <option value="GYEONGNAM">경상남도</option>
+            <option value="JEJU">제주특별자치도</option>  
+          </select>
+          <select name="keyWord ">
+              <option value="">키워드</option>
+              <option value="">1</option>
+              <option value="">2</option>
+              <option value="">3</option>
+              <option value="">4</option>
+              <option value="">5</option>  
+              
+          </select>
+          <select name="reviewScore">
+              <option value="">평점순</option>
+              <option value="">(4.5)⭐⭐⭐⭐☆</option>
+              <option value="">(4.0)⭐⭐⭐⭐  </option>
+              <option value="">(3.5)⭐⭐⭐☆  </option>
+              <option value="">(3.0)⭐⭐⭐  </option>
+              <option value="">(2.5)⭐⭐☆  </option>
+              
+              
+          </select>
 
       </div>
       
