0560fe0b7a4fffe3d4a31f9fc0fbc397038a0794
hosung jang <dndud0995@naver.com>
2025-05-09 21:47:54 +0900

    save

--- react_Project/src/components/MentorSearch.jsx
+++ react_Project/src/components/MentorSearch.jsx
@@ -10,14 +10,16 @@ const MentorSearch = () => {
     </div>
     <div className="ContainerSearch">
       <div className="searchRow">
-        <input type="text" placeholder="멘토 검색"/>
-        <button>검색</button>
+        <input className ="SearchInput" type="text" placeholder="멘토 검색"/>
+        <button className="btn Search-btn">검색</button>
       </div>
       <div className="filterOptions">
         <span>지역</span>
         <span>주제</span>
         <span>정렬</span>
       </div>
+      
+
     </div>
 
   </div>);
