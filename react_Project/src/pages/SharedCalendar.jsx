ecfc2a2d77a4346729ab99425a458a1a787ae2c4
DongEun02 <ehddms0517@naver.com>
2025-05-09 23:32:59 +0900

    Calendar setting

---
+++ react_Project/src/pages/SharedCalendar.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import TitleNav from "../components/TitleNav";
+import Calendar from "../components/Calendar";
+
+const Calender = () => {
+  return (
+    <>
+      <Header />
+      <TitleNav />
+      <main>
+        <Calender />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Calender;
