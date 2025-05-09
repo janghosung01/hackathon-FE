fa2bf7cbb4878ce8f35f61dcb44de19a9f9d6c47
DongEun02 <ehddms0517@naver.com>
2025-05-10 07:22:36 +0900

    CSS 수정

--- react_Project/src/App.jsx
+++ react_Project/src/App.jsx
@@ -10,7 +10,7 @@ import { Routes, Route } from "react-router-dom";
 import { createContext } from "react";
 
 import MentorSearch from "./components/MentorSearch"; // 경로는 프로젝트에 맞게 수정
-import MentorList from "./components/MentorList";     // 결과를 보여줄 컴포넌트 예시
+import MentorList from "./components/MentorList"; // 결과를 보여줄 컴포넌트 예시
 
 const mockData = [
   {
@@ -96,14 +96,12 @@ const mockData = [
 export const MentorInfoContext = createContext();
 
 function App() {
-
-   const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleFilterChange = (results) => {
     setSearchResults(results); // MentorSearch에서 전달된 결과 저장
   };
-  console.log("App전달",searchResults)
-
+  console.log("App전달", searchResults);
 
   return (
     <>
